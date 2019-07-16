663e5014baaeb10c5bd38979c543d477222e7801
hungpn <hungpn@nal.vn>
2019-07-16 23:16:35 +0700

    hng upload project

---
+++ app/src/main/java/com/land1812/friendly/MainActivity.java
@@ -0,0 +1,14 @@
+package com.land1812.friendly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
